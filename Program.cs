932395744f3fa0743103a5a06e56fc66ea63bc07
DucNM2004 <minhduc8883@gmail.com>
2024-06-24 09:35:49 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2ThucTap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choie;
+            do
+            {
+                Console.WriteLine("=========menu==========");
+                Console.WriteLine("1. chuc nang mot");
+                Console.WriteLine("2. chuc nang hai");
+                Console.WriteLine("3. chuc nang ba");
+                Console.WriteLine("4. chuc nang bon");
+                Console.WriteLine("5. chuc nang nam");
+                Console.WriteLine("6. chuc nang sau");
+                Console.Write("Moi ban chon chuc nang: ");
+                choie = int.Parse(Console.ReadLine());
+                switch (choie)
+                {
+                    case 1:
+                        Bai1();
+                        break;
+                    case 2:
+                        Bai2();
+                        break;
+                    case 3:
+                        Bai3();
+                        break;
+                    case 4:
+                        Bai4();
+                        break;
+                    case 5:
+                        Bai5();
+                        break;
+                    case 6:
+                        Bai6();
+                        break;
+                    case 0:
+                        Console.WriteLine("Ket thuc truong trinh");
+                        break;
+                    default:
+                        Console.WriteLine("chuc nang khong hop le");
+                        break;
+                }
+            }while (choie != 0);
+        }
+        static void Bai1()
+        {
+            QLMN qLMN = new QLMN();
+            int number;
+            do
+            {
+                Console.Write("Moi Ban nhap vao chu so ");
+                number = int.Parse(Console.ReadLine());
+                if(number < 100 || number > 999) 
+                {
+                    Console.WriteLine("Ban chi duoc nhap so co 3 chu so");
+                }
+            }while(number < 100 || number>999);
+            qLMN.Bai1(number);
+            
+        }
+        static void Bai2()
+        {
+            QLMN qLMN = new QLMN();
+            int number;
+            do
+            {
+                Console.Write("Moi Ban nhap vao chu so ");
+                number = int.Parse(Console.ReadLine());
+                if (number < 100 || number > 999)
+                {
+                    Console.WriteLine("Ban chi duoc nhap so co 3 chu so");
+                }
+            } while (number < 100 || number > 999);
+            qLMN.Bai2(number);
+
+        }
+        static void Bai3()
+        {
+            QLMN qLMN = new QLMN();
+            int number;
+            int number2;
+            do
+            {
+                Console.Write("Moi ban nhap vao thang: ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write("Moi ban nhap vao nam: ");
+                number2 = int.Parse(Console.ReadLine());
+                if (number < 1 || number > 12)
+                {
+                    Console.WriteLine("Ban chi duoc nhap thang hop le");
+                }
+            } while (number < 1 || number > 12);
+            qLMN.Bai3(number,number2);
+
+        }
+        static void Bai4()
+        {
+            QLMN qLMN = new QLMN();
+            int number;
+            do
+            {
+                Console.Write("Moi ban nhap vao so nguyen duong: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Ban chi duoc nhap so nguyen duong");
+                }
+            } while (number < 0);
+            string result = qLMN.Bai4(number);
+            Console.WriteLine($"So nguyen {number} sau khi chuyen doi thanh so nhi phan la: {result}");
+
+        }
+        static void Bai5()
+        {
+            QLMN qLMN = new QLMN();
+            int number;
+            do
+            {
+                Console.Write("Moi ban nhap vao so thu hang trong day fibonacci: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Ban chi duoc nhap so nguyen duong");
+                }
+            } while (number < 0);
+            int result = qLMN.Bai5(number);
+            Console.WriteLine($"gia tri tai so hang {number} la: {result}");
+
+        }
+        static void Bai6()
+        {
+            QLMN qLMN = new QLMN();
+                Console.Write("Nhap bac cua da thuc: ");
+                int bac = int.Parse(Console.ReadLine());
+
+                double[] heSo = new double[bac + 1];
+                for (int i = 0; i <= bac; i++)
+                {
+                    Console.Write($"Nhap he so a{i}: ");
+                    heSo[i] = double.Parse(Console.ReadLine());
+                }
+
+                Console.Write("Nhap gia tri x: ");
+                double x = double.Parse(Console.ReadLine());
+
+                double giaTri = qLMN.Bai6(x, bac, heSo);
+                Console.WriteLine($"Gia tri cua da thuc P(x) tai x = {x} la: {giaTri}");
+        }
+    }
+}
---
+++ QLMN.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2ThucTap
+{
+    internal class QLMN
+    {
+        public void Bai1(int x)
+        {
+            
+            int hangtram = x / 100; // 291/100 = 2 (hang tram)
+            int hangchuc = (x / 10) % 10; // 291/10 = 29%10 = 9 (hang chuc)
+            int hangdonvi = x % 10; // 291%10 = 1(hang don vi)
+
+            int Solonnhat = hangtram;
+            string viTri= "Hang tram";
+            if(Solonnhat < hangchuc)
+            {
+                Solonnhat = hangchuc;
+                viTri = "Hang chuc";
+            }
+            if (Solonnhat < hangdonvi)
+            {
+                Solonnhat = hangdonvi;
+                viTri = "Hang don vi";
+            }
+            Console.WriteLine($"So lon nhat co gia tri {Solonnhat} nam ow vi tri {viTri}");
+        }
+        public void Bai2(int x)
+        {
+            int hangtram = x / 100;
+            int hangchuc = (x/10) % 10;
+            int hangdonvi = x%10;
+
+            int[] mangso = { hangtram, hangchuc, hangdonvi };
+            for (int i = 0; i < mangso.Length - 1; i++)
+            {
+                for(int j = i+1; j < mangso.Length; j++)
+                {
+                    if (mangso[i]> mangso[j])
+                    {
+                        int temp = mangso[i];
+                        mangso[i] = mangso[j];
+                        mangso[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($" Chu so sau khi da sap xep: {mangso[0]}{mangso[1]}{mangso[2]}");
+        }
+        public bool NamNhuan(int x)
+        {
+            return((x % 4 == 0 && x % 100 != 0) || (x % 400 == 0));
+        }
+        public void Bai3(int thang,int nam) 
+        {
+            int soNgay = 0;
+            switch(thang)
+            {
+                case 1: 
+                case 3: 
+                case 5: 
+                case 7: 
+                case 8: 
+                case 10:
+                case 12: 
+                    soNgay = 31;
+                    break;
+                case 4: 
+                case 6: 
+                case 9: 
+                case 11:
+                    soNgay = 30;
+                    break;
+                case 2: 
+                    if (NamNhuan(nam))
+                    {
+                        soNgay = 29;
+                    }
+                    else
+                    {
+                        soNgay = 28;
+                    }
+                    break;
+            }
+            Console.WriteLine($"So ngay co trong thang {thang} cua nam {nam} la {soNgay} ngay");
+        }
+        public string Bai4(int x)
+        {
+            string ketqua = "";
+
+            while(x>0)
+            {
+                int phandu = x % 2;
+                ketqua = phandu.ToString() + ketqua;
+                x = x / 2;
+            }
+            return ketqua;
+        }
+        public int Bai5(int x)
+        {
+            if(x == 1 || x == 2)
+            {
+                return 1;
+            }
+            int f1 = 1;
+            int f2 = 1;
+            int fk = 0;
+            for (int i = 2; i < x; i++)
+            {
+                fk = f2 + f1;
+                f1 = f2;
+                f2 = fk;
+            }
+            return fk;
+        }
+        public double Bai6(double x,int bac, double[] heso)
+        {
+            double giatri = 0;
+            for(int i = 0; i <= bac; i++)
+            {
+                giatri += (heso[i] * Math.Pow(x, bac - i));
+            }
+            return giatri;
+        }
+    }
+}
